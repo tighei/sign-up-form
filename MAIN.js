96e2cd5752a2ad1bdddac5bba3543e806f0b0cb1
tighei <oussama.adame12@gmail.com>
2023-02-26 00:29:25 +0100

    a new project (sing up form ) by odin project

---
+++ MAIN.js
@@ -0,0 +1,12 @@
+var passConfirm = function () {
+  if (
+    document.getElementById("password").value ==
+    document.getElementById("password-con").value
+  ) {
+    document.getElementById("Message").style.color = "Green";
+    document.getElementById("Message").innerHTML = "Passwords match!";
+  } else {
+    document.getElementById("Message").style.color = "Red";
+    document.getElementById("Message").innerHTML = "Passwords do NOT match!";
+  }
+};

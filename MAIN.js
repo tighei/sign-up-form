3299f0a04ef3017d78a91374ec54e9e6ad463eaa
tighei <oussama.adame12@gmail.com>
2023-05-11 17:13:56 +0100

    change colors an some js

--- MAIN.js
+++ MAIN.js
@@ -5,7 +5,16 @@ let passConfirm = function () {
   ) {
     document.getElementById('Message').style.color = 'Green';
     document.getElementById('Message').innerHTML = 'Passwords match!';
-  } else {
+  } else if (
+    document.getElementById('password').value &&
+    document.getElementById('password-con').value === ''
+  ) {
+    document.getElementById('Message').style.color = 'blue';
+    document.getElementById('Message').innerHTML = 'password should be match';
+  } else if (
+    document.getElementById('password').value !==
+    document.getElementById('password-con').value
+  ) {
     document.getElementById('Message').style.color = 'Red';
     document.getElementById('Message').innerHTML = 'Passwords do NOT match!';
   }

7ebf6112bcaf3436867e336ec209ce55eb8ab727
tighei <123456789+odin@users.noreply.github.com>
2023-07-09 18:12:09 +0100

    add a regex to  password

--- MAIN.js
+++ MAIN.js
@@ -1,21 +1,32 @@
-let passConfirm = function () {
-  if (
-    document.getElementById('password').value ===
-    document.getElementById('password-con').value
-  ) {
-    document.getElementById('Message').style.color = 'Green';
-    document.getElementById('Message').innerHTML = 'Passwords match!';
-  } else if (
-    document.getElementById('password').value &&
-    document.getElementById('password-con').value === ''
-  ) {
-    document.getElementById('Message').style.color = 'blue';
-    document.getElementById('Message').innerHTML = 'password should be match';
-  } else if (
-    document.getElementById('password').value !==
-    document.getElementById('password-con').value
-  ) {
-    document.getElementById('Message').style.color = 'Red';
-    document.getElementById('Message').innerHTML = 'Passwords do NOT match!';
+const SubmitBtn = document.getElementById('submit-btn');
+const password = document.querySelector('#password');
+const repassword = document.querySelector('#password-con');
+const Message = document.getElementById('Message');
+
+function check() {
+  let reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  if (password.value.match(reg) && repassword.value.match(reg)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const passConfirm = function () {
+  if (password.value === repassword.value) {
+    Message.style.color = 'green';
+    Message.innerHTML = 'Passwords match!';
+  } else if (password.value && repassword.value === '') {
+    Message.style.color = 'blue';
+    Message.innerHTML = 'Password should be matched';
+  } else {
+    Message.style.color = 'red';
+    Message.innerHTML = 'Passwords do NOT match!';
   }
 };
+let sub = SubmitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (check()) {
+    passConfirm();
+  }
+});

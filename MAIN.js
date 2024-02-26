1bf7d16a83ebb99193ef1df99f78f28cdbedb74e
tighei <oussama.adame12@gmail.com>
2024-02-26 16:17:29 +0100

    Refactor CSS and JavaScript code

--- MAIN.js
+++ MAIN.js
@@ -1,33 +1,41 @@
-const SubmitBtn = document.getElementById('submit-btn');
-const password = document.querySelector('#password');
-const repassword = document.querySelector('#password-con');
-const Message = document.getElementById('Message');
+const SubmitBtn = document.getElementById("submit-btn");
+const password = document.querySelector("#password");
+const repassed = document.querySelector("#password-con");
+const Message = document.getElementById("Message");
 
-function check() {
-  let reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-  if (password.value.match(reg) && repassword.value.match(reg)) {
-    return true;
-  } else {
-    return false;
-  }
+async function check() {
+    let reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    if (password.value.match(reg) && repassed.value.match(reg)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const passConfirm = function () {
-  if (password.value === repassword.value) {
-    Message.style.color = 'green';
-    Message.innerHTML = 'Passwords match!';
-  } else if (password.value && repassword.value === '') {
-    Message.style.color = 'blue';
-    Message.innerHTML = 'Password should be matched';
-  } else {
-    Message.style.color = 'red';
-    Message.innerHTML = 'Passwords do NOT match!';
-  }
-};
-let sub = SubmitBtn.addEventListener('click', (e) => {
-  if (check()) {
-    passConfirm();
-  } else {
-    e.preventDefault();
-  }
+function passConfirm() {
+    if (password.value === "" && repassed.value === "") {
+        Message.style.color = "blue";
+        Message.textContent = "Password should be matched";
+        return false;
+    } else if (password.value === repassed.value) {
+        Message.style.color = "green";
+        Message.textContent = "Passwords match!";
+        return true;
+    } else {
+        Message.style.color = "red";
+        Message.textContent = "Passwords do NOT match!";
+        return false;
+    }
+}
+function checkMatch() {
+    password.addEventListener("input", passConfirm);
+    repassed.addEventListener("input", passConfirm);
+}
+checkMatch();
+SubmitBtn.addEventListener("click", (e) => {
+    if (check() && passConfirm()) {
+        true;
+    } else {
+        e.preventDefault();
+    }
 });
